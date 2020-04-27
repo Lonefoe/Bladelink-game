3679d5116e331d0d826d157236ab07707842a717
Phantomcryy <54005587+Phantomcryy@users.noreply.github.com>
2020-04-27 10:55:02 +0200

    Added cloud, rock sprites

---
+++ Soulbound/Assets/ScrollingObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    [SerializeField] float scrollSpeed = 5f;
+
+    private void Update()
+    {
+        float scrollTo = transform.position.x + scrollSpeed * Time.deltaTime;
+
+        transform.position = new Vector2(scrollTo, transform.position.y);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Debug.Log("invisible");
+    }
+}
